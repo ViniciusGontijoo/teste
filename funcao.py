58eeb348b7d0cb9b0c8e1e83b4b0d2ace29e1da7
ViniciusGontijoo <gontijovinicius790@gmail.com>
2025-10-29 17:39:59 -0300

    Teste de funções

---
+++ funcao.py
@@ -0,0 +1,16 @@
+def SomarNumeros(a,b):
+    return a + b
+
+resultado = SomarNumeros(4,2)
+print(resultado)
+
+def saudacao(nome="Visitante"):
+    print(f"Olá, {nome} seja bem-vindo!")
+
+saudacao()
+saudacao("Vinícius")
+
+for i in range(1,10):
+    if i == 7 or i == 6:
+        break
+    print(i)
